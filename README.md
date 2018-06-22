9758acbe89eb248140fd1c7b37a4f19d48c8189b
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-06-23 03:46:43 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<img src="https://github.com/JackMD/AutoXP/blob/master/meta/AutoXP.png">
+
+# AutoXP
+
+| HitCount | License | Poggit | Release |
+|:--:|:--:|:--:|:--:|
+|[![HitCount](http://hits.dwyl.io/JackMD/AutoXP.svg)](http://hits.dwyl.io/JackMD/AutoXP)|[![GitHub license](https://img.shields.io/github/license/JackMD/AutoXP.svg)](https://github.com/JackMD/AutoXP/blob/master/LICENSE)|[![Poggit-CI](https://poggit.pmmp.io/ci.shield/JackMD/AutoXP/AutoXP)](https://poggit.pmmp.io/ci/JackMD/AutoXP/AutoXP)|[![](https://poggit.pmmp.io/shield.state/AutoXP)](https://poggit.pmmp.io/p/AutoXP)|
+
+### A AutoXP plugin for PocketMine-MP // McPe 1.2
+### Features:
+ - AutoXP automatically transfers all the xp to your experience bar without dropping any.
+ - Great way to reduce lag caused by Experience Orbs.
+### How to setup?
+ - Get the [.phar](https://poggit.pmmp.io/ci/JackMD/AutoXP/AutoXP) and drop the into your `plugins` folder.
+ - Start the server.
+ - Start mining blocks.
+### Info:
+  - Make sure to subscribe to be updated for when i release more stuff on my [YT](https://youtu.be/x_mc-ocrdDU) channel.
+  - Support is appreciated.
+  - Please don't hesitate to ask questions or report bug report in issues section.
+### Disclaimer:
+
+```
+This software is licensed under "GNU General Public License v3.0".
+This license allows you to use it and/or modify it but you are not at
+all allowed to sell this plugin at any cost. If found doing so the
+necessary action required would be taken. Further removal of the License and or
+authors name from this software is strictly prohibited.
+```
---
+++ src/JackMD/AutoXP/Main.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ *   ___        _       __   ________
+ *  / _ \      | |      \ \ / /| ___ \
+ * / /_\ \_   _| |_ ___  \ V / | |_/ /
+ * |  _  | | | | __/ _ \ /   \ |  __/
+ * | | | | |_| | || (_) / /^\ \| |
+ * \_| |_/\__,_|\__\___/\/   \/\_|
+ *
+ * Discord: JackMD#3717
+ * Twitter: JackMTaylor_
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
+ *
+ * AutoXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ * ------------------------------------------------------------------------
+ */
+
+namespace JackMD\AutoXP;
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents(($this), $this);
+		$this->getLogger()->info("Plugin Enabled.");
+	}
+	
+	public function onBreak(BlockBreakEvent $event){
+		$event->getPlayer()->addXp($event->getXpDropAmount());
+		$event->setXpDropAmount(0);
+	}
+	
+	public function onPlayerKill(PlayerDeathEvent $event){
+		$player = $event->getPlayer();
+		$cause = $player->getLastDamageCause();
+		if($cause instanceof EntityDamageByEntityEvent){
+			$damager = $cause->getDamager();
+			if($damager instanceof Player){
+				$damager->addXp($player->getXpDropAmount());
+				$player->setCurrentTotalXp(0);
+			}
+		}
+	}
+}
