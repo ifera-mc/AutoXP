25037153b26eac69f4f87d27a998beb53fa7e1a9
95CivicSi <aaron_salyer@yahoo.com>
2018-11-10 03:10:14 -0500

    Re-Format to match original.

--- src/JackMD/AutoXP/Main.php
+++ src/JackMD/AutoXP/Main.php
@@ -36,39 +36,39 @@ use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener{
-	
-	public function onEnable(): void{
+class Main extends PluginBase implements Listener {
+
+	public function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents(($this), $this);
 		$this->getLogger()->info("Plugin Enabled.");
 	}
 
-    /**
-     * @param BlockBreakEvent $event
-     * @priority HIGHEST
-     */
-	public function onBreak(BlockBreakEvent $event){
-	    if($event->isCancelled()){
-	        return;
-        }
+	/**
+	 * @param BlockBreakEvent $event
+	 * @priority HIGHEST
+	 */
+	public function onBreak(BlockBreakEvent $event) {
+		if ($event->isCancelled()) {
+			return;
+		}
 		$event->getPlayer()->addXp($event->getXpDropAmount());
 		$event->setXpDropAmount(0);
 	}
 
 
-    /**
-     * @param PlayerDeathEvent $event
-     * @priority HIGHEST
-     */
-	public function onPlayerKill(PlayerDeathEvent $event){
-	    if($event->isCancelled()){
-	        return;
-        }
+	/**
+	 * @param PlayerDeathEvent $event
+	 * @priority HIGHEST
+	 */
+	public function onPlayerKill(PlayerDeathEvent $event) {
+		if ($event->isCancelled()) {
+			return;
+		}
 		$player = $event->getPlayer();
 		$cause = $player->getLastDamageCause();
-		if($cause instanceof EntityDamageByEntityEvent){
+		if ($cause instanceof EntityDamageByEntityEvent) {
 			$damager = $cause->getDamager();
-			if($damager instanceof Player){
+			if ($damager instanceof Player) {
 				$damager->addXp($player->getXpDropAmount());
 				$player->setCurrentTotalXp(0);
 			}
